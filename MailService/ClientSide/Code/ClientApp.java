a703ad64c2a7cd1b27653873eae24767ff17edb3
Luca Del Conte <delconteluca55@gmail.com>
2024-10-17 11:04:16 +0200

    OK, Setted All

--- MailService/ClientSide/Code/ClientApp.java
+++ MailService/ClientSide/Code/ClientApp.java
@@ -1,2 +1,21 @@
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+public class ClientApp extends Application {
 
+    public static Model model;
+    public static SceneController sceneController;
+    private static ExecutorService appFX;
+    private static ScheduledExecutorService fetchEmails;
+    private static Date lastFetch = new Date(Long.MIN_VALUE);
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Scene scene = new Scene(new Pane(), 1280, 720);
+        //scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
+        sceneController = SceneController.getInstance(scene);
+        sceneController.addScene(SceneName.Main);
+        sceneController.addScene(SceneName.MailCompose);
