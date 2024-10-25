70b68e8fc1ebdbe23b53bf2d547382da372f0962
Luca Del Conte <delconteluca55@gmail.com>
2024-10-25 10:55:59 +0200

    Attempt to make graphics work

--- MailService/ServerSide/src/main/java/mailservice/serverside/ServerApp.java
+++ MailService/ServerSide/src/main/java/mailservice/serverside/ServerApp.java
@@ -1,2 +1,24 @@
 package mailservice.serverside;
 
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ServerApp extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(mailservice.serverside.HelloApplication.class.getResource("ServerLog.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
