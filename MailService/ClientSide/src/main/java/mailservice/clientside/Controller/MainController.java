591d1b570838a1161b329c809cdd65357921a3eb
Alexandrina Budeanu <120032764+alexaorsasha@users.noreply.github.com>
2024-10-30 00:25:11 +0100

    MainController Handlers test

--- MailService/ClientSide/src/main/java/mailservice/clientside/Controller/MainController.java
+++ MailService/ClientSide/src/main/java/mailservice/clientside/Controller/MainController.java
@@ -7,9 +7,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;  //importo la classe Label
 import javafx.scene.control.ListView;
-import javafx.scene.web.WebView;    //importo la classe WebView, che visualizza contenuti web
+import javafx.scene.web.WebView;//importo la classe WebView, che visualizza contenuti web
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import mailservice.clientside.ClientApp;
+import java.io.IOException;
 
 public class MainController {
     //collegamento con la GUI tramite l'annotazione @FXML
@@ -24,7 +26,7 @@ public class MainController {
     @FXML
     private WebView MailContent; //serve a visualizzare il contenuto dell'email
     @FXML
-    private ListView MailList; //serve a visualizzare la lista delle email
+    private ListView<String> MailList; //serve a visualizzare la lista delle email
     @FXML
     private Label MailLabel; //serve a visualizzare la mail email
 
@@ -33,18 +35,43 @@ public class MainController {
     //metodo che viene chiamato quando si preme il bottone
     @FXML
     protected void onComposeButtonClick() {
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/mailservice/clientside/MailCompose.fxml"));
+            Parent composeView = loader.load();
+            Scene composeScene = new Scene(composeView);
+            Stage composeStage = new Stage();
+
+            composeStage.setScene(composeScene);
+            composeStage.setTitle("Compose your Email");
+            composeStage.initModality(Modality.APPLICATION_MODAL); //serve per permettere all'utente di interagire con questa finestra prima di tornare alla finestra principale
+            composeStage.show();
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
     @FXML
+    //handler per l'azione del bottone Compose
     protected void onComposeButtonAction() {
-
+        System.out.println("Composing a new Email...");
+        onComposeButtonClick();
     }
     @FXML
     protected void onDeleteButtonClick() {
-
+        int selectedIndex =MailList.getSelectionModel().getSelectedIndex(); //
+        if(selectedIndex>=0){//se >=0 vuol dire che la mail è stata selezionata
+            MailList.getItems().remove(selectedIndex);
+            System.out.println("Email deleted from the list");
+        }else{
+            System.out.println("No Email selected to delete");
+        }
     }
     @FXML
+    //handler per l'azione del bottone Delete
     protected void onDeleteButtonAction() {
+        System.out.println("Delete button action triggered.");
+        onDeleteButtonClick();
 
     }
     @FXML
