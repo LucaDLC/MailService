bc21870a593ca42dc49ad1232681db5436c811dd
Luca Del Conte <delconteluca55@gmail.com>
2024-10-29 09:58:48 +0100

    Changed Video Settings

--- MailService/ClientSide/src/main/java/mailservice/clientside/ClientApp.java
+++ MailService/ClientSide/src/main/java/mailservice/clientside/ClientApp.java
@@ -21,7 +21,7 @@ public class ClientApp extends Application {
     public void start(Stage stage) throws IOException {
         //start(Stage stage) è il metodo principale di un'applicazione JavaFX, che viene chiamato quando l'applicazione viene avviata
         FXMLLoader fxmlLoader = new FXMLLoader(ClientApp.class.getResource("/mailservice/clientside/Main.fxml")); //crea un oggetto per carica il file FXML
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 720); //crea la scena con il contenuto del file FXML
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 640); //crea la scena con il contenuto del file FXML
         stage.setTitle("ClientSide - App"); //imposta il titolo della finestra
         stage.setScene(scene); //associa la scena(l'interfaccia utente) alla finestra
         stage.show();
