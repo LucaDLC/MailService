01c501e518d9dc2afed47c3772803e9ae4a2ba78
Luca Del Conte <delconteluca55@gmail.com>
2024-11-26 10:45:59 +0100

    Removed thing that we don't need

--- MailService/ClientSide/src/main/java/mailservice/clientside/Model/ClientModel.java
+++ MailService/ClientSide/src/main/java/mailservice/clientside/Model/ClientModel.java
@@ -1,7 +1,6 @@
 package mailservice.clientside.Model;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -87,7 +86,6 @@ public class ClientModel {
         {
             configManager.setProperty("Client.Mail", email);
             this.userLogged = email;
-            CartellaCreazione(email);
             sendLogicRequest("CheckMail",email);
         }
         return checkMail;
@@ -147,25 +145,6 @@ public class ClientModel {
         }
         System.out.println("Error: Not connected to the server or output stream is null");
         return false;
-    }
-
-    private void CartellaCreazione (String email) {
-
-        // Percorso in cui cercare la cartella
-        String path = new File("").getAbsolutePath() + File.separator + "ClientSide" + File.separator + "src" + File.separator + "main" + File.separator + "java" + File.separator + "mailservice" + File.separator + "clientside" + File.separator + "Customers" + File.separator + email;
-        File cartella = new File(path);
-
-        if (!cartella.exists()) {
-            // Se la cartella non esiste, la crea
-            if (cartella.mkdir()) {
-                System.out.println("Cartella creata con successo: " + email);
-            } else {
-                System.out.println("Errore nella creazione della cartella.");
-            }
-        } else {
-            System.out.println("La cartella esiste già: " + email);
-        }
-
     }
 
 }
